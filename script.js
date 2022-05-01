39c8bf2f09c28c9e2e6ceb114b262075aafdf380
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2022-05-01 14:58:49 +0400

    Added database implementation

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const Q1 = document.getElementById("Q1");
 const Q2 = document.getElementById("Q2");
 const Q3 = document.getElementById("Q3");
+// You don't need those btw ^
 const submitButton = document.getElementById("submit-btn");
+// this ^ returns null in index.html because there's no submitButton there
+
+import { submitScore, loadScores } from "./database.js";
+// Use these functions anywhere ^
+
+// submitScore(<string>name, <number>score);
+
+// loadScores();
+// returns an array of objects with all the scores (sorted from highest to lowest score)
+// e.g. [{name: "Psycho", score: 3}, {name: "Dxl", score: 4},...]
+// IMPORTANT: this function is asynchronous, meaning you need to tell the browser to wait
+// for the response from the server, you can do this with either the "await" or "then" keywords
+// as follows:
+// const leaderboards = await loadScores();
+// then you can use the variable leaderboards as you wish
+// Or:
+// loadScores().then((scores) => ( ...do something with the variable scores here...))
+// They're essentially the same...
 
 
 const correctAnswers = {
@@ -34,3 +53,8 @@ function nextQuestion() {
   }
   alert("YOU NEED TO PICK AN ANSWER");
 }
+
+// TODO: Make an onClick function for the anchor tag that starts the quiz at first,
+// assuming you will have the name input right with the button, you'll need a way
+// to save the name in JS because it will get lost on page transition... (You'll
+// probably have to change the <a> into a <button> or something...)
